8a591c1593c3d6479392a3d8fecb4abef207c4fd
zac <zgallagher29@gmail.com>
2019-03-25 15:37:27 -0500

    rejects when the first word has a duplicate

--- HW7/prod/main.py
+++ HW7/prod/main.py
@@ -17,36 +17,71 @@ class Transition():
 
 def main():
     t = Tape('')
-    t2 = Tape('this will fail yay')
+    t2 = Tape('#this#this')
+
     runSimulation(t.tape)
     print('--------------')
     runSimulation(t2.tape)
 def runSimulation(newTape):
     pos = 0
+    newTape.append('|_|')
     state1(newTape,pos)
 
 def state1(word,pos):
     print('State 1')
-    if(len(word)==0):
+
+    if(len(word)==1):
         print('empty string')
-        accept('1', '|..|')
+        accept('1', '|_|')
     elif(word[pos]=='#'):
-      
-       
-        word[pos]
-        pos =pos+1
+        
+        transition1to2(word, pos)
     else:
         reject('1', word)
 
+def transition1to2(word,pos):
+    print('Transition # ---> X , R')
+    word[pos]='X'
+    pos = pos +1
+    state2(word, pos)
+def transition2to3(word,pos):
+    print('Transition '+word[pos] +' --->'+word[pos]+' , R')
+    pos = pos +1
+    state3(word, pos)
+    
+def state2(word, pos):
+    print('State 2')
+    printLocation(pos, word)
+    transition2to3(word,pos)
+
+def state3(word,pos):
+    print('State 3')
+    for x in range(pos,len(word)):
+        printLocation(x, word)
+        if word[pos-1]==word[x]:
+            reject('3',word)
+            break
+        elif word[x]=='|_|':
+            transiton3to4(word,x)
+def transiton3to4(word, pos):
+    print('Transition '+word[pos] +' ---> L')
+    pos = pos-1
+
+
+
+
+
+def printLocation(pos, word):
+    print('Looking at '+word[pos]+ ' at index '+str(pos))
 
 
 def accept(previousState,word):
         print('ACCEPT AFTER STATE:'+ previousState )
-        print('Final Output of Word: ')
+        print('Final Output of Tape: ')
         print(word)
 def reject(previousState,word):
         print('REJECT AFTER STATE: ' + previousState)
-        print('Final Output of Word: ')
+        print('Final Output of Tape: ')
         print(word)
     
 def setUpTape(initWord):
