dd4aee6f4f8caba4816bbaf12bb6eaaf0052c77e
zac <zgallagher29@gmail.com>
2019-03-25 13:36:29 -0500

    state 1 rejects when word is entered before # and accepts on empty string

--- HW7/prod/main.py
+++ HW7/prod/main.py
@@ -8,25 +8,42 @@ class Tape():
         self.tape = setUpTape(initialString)
  
     
+class Transition():
+       def __init__(self, curChar, direction,replaceChar):
+    
+        self.curChar= curChar
+        self.replaceChar = replaceChar
+        self.direction = direction
 
 def main():
     t = Tape('')
     t2 = Tape('this will fail yay')
     runSimulation(t.tape)
+    print('--------------')
     runSimulation(t2.tape)
 def runSimulation(newTape):
     pos = 0
     state1(newTape,pos)
 
 def state1(word,pos):
-   
+    print('State 1')
     if(len(word)==0):
+        print('empty string')
         accept('1', '|..|')
     elif(word[pos]=='#'):
-        print('working on it')
+      
+       
+        word[pos]
+        pos =pos+1
     else:
         reject('1', word)
-        
+def state2():
+
+
+def transition1to2():
+     print('Transition: # --> X, move right')
+
+
 def accept(previousState,word):
         print('ACCEPT AFTER STATE:'+ previousState )
         print('Final Output of Word: ')
