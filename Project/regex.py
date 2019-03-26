02f4854cb7467a4c5d109fc719ed2f792e1cd556
zac <zgallagher29@gmail.com>
2019-03-26 14:16:51 -0500

    project. create regex that identifies mentions and emails

---
+++ Project/regex.py
@@ -0,0 +1,25 @@
+import re
+
+patterns = {'email':'^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$', 'mention': '@([^:\s]+)'}
+
+
+def main():
+    userInput = '1'
+    while userInput == '1':
+        userInput = input('Enter your string you would like to input: ')
+        runChecks(userInput)
+        userInput = input('Enter 1 to check another string or 0 to quit: ')
+
+def runChecks(inputRun):
+   for key, value  in patterns.items(): 
+       if checkValid(value,inputRun): print(inputRun + ' is a valid '+ key)
+
+def checkValid(pattern,checkInput ):
+    return re.match(pattern,checkInput )
+
+
+       
+
+
+
+main()
