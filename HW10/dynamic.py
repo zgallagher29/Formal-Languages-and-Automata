1e202578c311ce696a3e1cf83d082386298f8cbf
zac <zgallagher29@gmail.com>
2019-04-14 13:00:38 -0500

    functional with small error

--- HW10/dynamic.py
+++ HW10/dynamic.py
@@ -3,25 +3,25 @@
 
 # In[1]:
 
-
+import time
 import numpy as np
 
-productions = 0
+
 
 
 
 # In[2]:
-
-
+prods = ['S->AB|BC','A->BA|a','B->CC|b','C->AB|a']
+nums = 4
 def main():
     
-    start = input("What is the Start Variable? ")
-    productions = int(input("What is the number of productions? "))
-  
+    
+  #  productions = int(input("What is the number of productions? "))
+    productions =4
     grammar = np.empty([productions, 12], dtype=object)
     for i in range(0, productions):
-        print("Enter a production with a space at the end. Example format: S->aSa|bSb|b|a .")
-        prod = input('HEY')
+        print("Enter a production. Example format: S->aSa|bSb|b|a .")
+        prod = prods[i]
       
         print(prod)
         grammar[i][0] = prod[0]  # before the - in '->'
@@ -35,37 +35,60 @@ def main():
             grammar[i][index] = term
             index = index +1
             
-            
+        grammar[i][index]="$"
+    
+    for i in range(0,productions):
+        for j in range(0,12):
+            if grammar[i][j] is None:
+                grammar[i][j]= ""
     print(grammar)
-    checkString(grammar)
+    checkString(grammar,productions)
     
-def checkString(grammar):
+def checkString(grammar,productions):
     
-    word = input('What is the string you would like to check? ')
-    
-    matrix = np.empty([len(word),len(word)], dtype = str)
+    #word = input('What is the string you would like to check? ')
+    word = "baaba"
+    matrix = np.empty([len(word),len(word)], dtype = object)
+    for x in range(len(word)):
+        for y in range(len(word)):
+            matrix[x][y]='x'
+    x=0
+    nonTerm = ""
+    for letter in word:
+       
+        for prod in range(0,productions):
+            right = 3
+            while grammar[prod][right]!='$':
+                if grammar[prod][right] ==letter:
+                    nonTerm = nonTerm+grammar[prod][0]
 
-    for i in range(0, len(word)):
+                right = right +1
         
+      
+        matrix[x][x] = nonTerm
         nonTerm = ""
-        st=""
-        st = st+word[i]
-        for j in range(0, productions):
-            l = 1 
-            while grammar[j][l] != "$":
-                  
-                if grammar[j][l] ==st:
-                    nonTerm = concat(nonTerm,grammar[j][0])
-                    
-                l = l+1
-        matrix[i][i]=nonTerm
-    for l in range(1,len(word)):
-        for j in range(l,len(word)):
+        x=x+1
+    print(matrix)
+    nonTerm=""
+    for k in range(1, len(word)):
+        for j in range(k, len(word)):
             nonTerm = ""
-            for m in range( j-l, j):
-                pr = combine(matrix[j-l][m],matrix[m+1][j],grammar)
-                nonTerm = concat(nonTerm,pr)
-            matrix[j-l][j]=nonTerm
+            for m in range(j-k, j):
+                print("j ", j)
+                print("k", k)
+                print("m", m)
+                print (matrix)
+                enter = input("Press enter to keep going")
+
+                pr = search_combo(matrix[j-k][m],matrix[m+1][j],grammar,productions)
+
+                nonTerm =nonTerm+pr
+            matrix[j-k][j] = nonTerm
+
+        # matrix row 2 two letters: check if row below  
+
+
+    
         ##print_results(matrix, word)
     print(matrix)
     if isValidString(word,matrix):
@@ -86,28 +109,35 @@ def isValidString(word, matrix):
     return False
                             
                         
-def combine(a,b,grammar):
+def search_combo(a,b,grammar,productions):
     pri=a
     re=""
     for i in range(0,len(a)):
         for j in range(0,len(b)):
             pri = ""
             pri = pri+a[i]+b[j]
-            re = re + find_production(pri,grammar)
+            print('HEPRLLFSA',pri)
+            re =re +find_production(pri,grammar,productions)
+            
     return re 
                     
-def find_production(p,grammar):
+def find_production(p,grammar,productions):
     r =""
+ 
+
     for i in range(0,productions):
-        l = 1 
+        l = 3 
         while grammar[i][l]!="$":
             if grammar[i][l] == p:
-                r = concat(r,grammar[i][0])
+                r=r + grammar[i][0]
+            l = l+1
+    return r
 def concat(nonTerm,gram):
     r = nonTerm
     for i in range(0,len(gram)):
         if r.find(gram[i]) > len(r):
-            r = r+gram[i]        
+            r = r+gram[i]   
+    print('CONCAT R ',r)     
     return r
         
         
