e324660bb69ab3ac73ed2c638eefba5be5c42243
zac <zgallagher29@gmail.com>
2019-04-22 21:56:45 -0500

    works with the hard coded 2CNF

--- HW11/cnf.py
+++ HW11/cnf.py
@@ -16,17 +16,18 @@ def main():
 
     resClause = []
     newClause = False
-    for c in clauses:
-        for j in range(1,len(clauses)-1):
-            for vc in range(0,len(c)):
+    for c in range(0,len(clauses)-1):
+        for j in range(1,len(clauses)):
+            for vc in range(0,len(clauses[c])):
                 for vj in range(0,len(clauses[j])):
-                    if c[vc] == clauses[j][vj]:
-              
-                        if (c[vc-1] =='|' and clauses[j][vj-1] !='|'):
+                    
+                    if clauses[c][vc] == clauses[j][vj]:
+                        print(clause[c][vc-1]+clauses[j][vj-1])
+                        if (clauses[c][vc-1] =='|' and clauses[j][vj-1] !='|'):
                             
-                            temp = c.replace(c[vc-1],"")
+                            temp = clauses[c].replace(clauses[c][vc-1],"")
                             temp = temp +clauses[j]
-                            temp = temp.replace(c[vc],"")
+                            temp = temp.replace(clauses[c][vc],"")
                         
                             resClause.append(temp)
                             temp =""
@@ -35,17 +36,23 @@ def main():
                             
 
 
-                        elif(c[vc-1] !='|' and clauses[j][vj-1] =='|'):
-                            temp = clauses[j].replace(clauses[j][vj-1],"")
-                            temp = temp +clauses[j]
-                            temp = temp.replace(c[vc],"")
+                        elif(clauses[c][vc-1] !='|' and clauses[j][vj-1] =='|'):
+                       
+                            temp = '('+clauses[j][:vj-1]+clauses[j][vj+1:]
+
+                            temp = temp +"V"+clauses[c][:vj]+clauses[c][vj+1:]+')'
+                         
+                            resClause.append(temp)
+                            temp =""
+                        
                             newClause = True
                            
         if newClause == False:
-            resClause.append(c)
+            resClause.append(clauses[c])
         else:
             newClause == False
-
+    if not resClause:
+        print('unsatified')
     print(resClause)
 
                         
